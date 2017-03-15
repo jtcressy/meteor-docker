779b0f6f2318f6309fc1c48121e72365fb9f9023
Joel Cressy <joel@jtcressy.net>
2017-03-15 16:41:29 -0600

    fix to improve mounted volume permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 sudo chown -Rh meteor:meteor /app
 cd /app
 meteor npm install --save babel-runtime
-meteor "$@"
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+
+exec /usr/local/bin/gosu user meteor "$@"
